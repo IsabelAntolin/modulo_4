0bd6095880c10387fee4c8e5dfa31e5975c52148
IsabelAntolin <isaabuu@gmail.com>
2022-06-30 17:56:23 -0400

    asincrónico clase 2 mod 4

--- ejercicios_asincronico/clase_01/script.js
+++ ejercicios_asincronico/clase_01/script.js
@@ -1,7 +1,7 @@
-let containerTareas = document.querySelector('#container');
 let inputTarea = document.querySelector('.input');
 let btnAgregarTarea = document.querySelector('.boton-agregar')
 let btnEliminarTarea = document.querySelector('.boton-remover')
+let containerTareas = document.querySelector('#container');
 
 let mensajeTarea;
 
@@ -11,8 +11,8 @@ function AgregarMensaje(){
     mensaje = `<div class="item">
                 <a href="#"><img src="img/flor.png" alt=""></a>
                 <input type="text" class="item-input" disabled value="${mensajeTarea}">
-                <a class="boton-editar"><img src="img/iconCandado.png" alt=""></a>
-                <a class="boton-remover"><img src="img/iconRemove.png" alt=""></a>
+                <a class="boton-editar"><i class="fa-solid fa-lock"></i></a>
+                <a class="boton-remover"><i class="fa-solid fa-circle-minus"></i></a>
   </div> `
     containerTareas.innerHTML =  containerTareas.innerHTML + mensaje
     mensajeTarea= inputTarea.value; 
@@ -20,7 +20,11 @@ function AgregarMensaje(){
 }
 
 btnAgregarTarea.addEventListener('click',function(){
-    AgregarMensaje()
+    if(!(((inputTarea.value).trim()) == '')){
+        AgregarMensaje()
+        inputTarea.value=''
+    }
+   
 })
 // btnEliminarTarea.addEventListener('click',function(){
 //     AgregarMensaje()
