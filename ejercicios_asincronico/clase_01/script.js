093685b1574f7f5b2a6e569111f9f767af04e09d
IsabelAntolin <isaabuu@gmail.com>
2022-07-08 22:31:19 -0400

    asincrónicos mod 4 terminados

--- ejercicios_asincronico/clase_01/script.js
+++ ejercicios_asincronico/clase_01/script.js
@@ -1,9 +1,6 @@
-//const { json } = require("express");
-
-const inputTarea = document.querySelector('.input');
+const inputTarea = document.querySelector('.input'); //input principal
 const btnAgregarTarea = document.querySelector('.boton-agregar')
 const containerTareas = document.querySelector('#container');
-
 let datosLocalStore;
 
 class Item {
@@ -12,6 +9,7 @@ class Item {
   }
   crearDiv(nuevaTarea) {
     const inputItem = document.createElement('input')
+    inputItem.type = 'text'
     inputItem.classList.add('item-input');
     inputItem.disabled = true;
     inputItem.value = nuevaTarea
@@ -19,13 +17,13 @@ class Item {
     const divItem = document.createElement('div')
     divItem.classList.add('item');
     //*********************************************/
-    const botonEditar = document.createElement('a')
-    botonEditar.classList.add('boton-editar')
+    const botonEditar = document.createElement('button')
     botonEditar.innerHTML = '<i class="fa-solid fa-lock"></i>'
+    botonEditar.classList.add('boton-editar')
     //*********************************************/
-    const botonRemover = document.createElement('a')
+    const botonRemover = document.createElement('button')
+    botonRemover.innerHTML = '<i class="fa-solid fa-trash-can"></i>'
     botonRemover.classList.add('boton-remover')
-    botonRemover.innerHTML = '<i class="fa-solid fa-circle-minus"></i>'
     //*********************************************/
     const logoFlor = document.createElement('a');
     logoFlor.innerHTML = '<img src="img/flor.png" alt="">'
@@ -37,22 +35,19 @@ class Item {
     containerTareas.appendChild(divItem)
 
     botonEditar.addEventListener('click', function () {
-      console.log(this)
+      // primer click desbloquear - segundo click bloquear     
       if (!inputItem.disabled) {
         botonEditar.innerHTML = '<i class="fas fa-lock"></i>'
         inputItem.disabled = true
-        return
+        editarLocalStore()
+      } else {
+        botonEditar.innerHTML = '<i class="fas fa-lock-open"></i>'
+        inputItem.disabled = false
       }
-      botonEditar.innerHTML = '<i class="fas fa-lock-open"></i>'
-      inputItem.disabled = false
     })
-
     botonRemover.addEventListener('click', function () {
-      console.log(this)
-      console.log(inputItem.value)
       divItem.remove()
-
-      eliminarLocalStore(inputItem.value)      
+      eliminarLocalStore(inputItem.value)
     })
   }
 }
@@ -66,66 +61,49 @@ function chequearInput() {
     let item = new Item(inputTarea.value)
     //Agregar a localStorage
     saveTask(inputTarea.value)
-    //
     inputTarea.value = ''
   }
 }
-
+/************bonus********** */
 function saveTask(task) {
-  //obtengo las Tareas
-  let localTasks = localStorage.getItem('tareas') || '[]'
-  localTasks = JSON.parse(localTasks)
-  localTasks.push(task);
-  localTasks = JSON.stringify(localTasks)
-  localStorage.setItem('tareas', localTasks)
+  ObtenerLocalStore() //obtiene los datos del localstore y los devuelve en un array
+  datosLocalStore.push(task); //agrego la tarea al array que llega como resultado de la funcion obtener
+  datosLocalStore = JSON.stringify(datosLocalStore) //transformo a string
+  localStorage.setItem('tareas', datosLocalStore) //agrego en localstore
 }
-
 function ObtenerLocalStore() {
   datosLocalStore = localStorage.getItem('tareas') || '[]'
   datosLocalStore = JSON.parse(datosLocalStore)
-  console.log(datosLocalStore)
   return datosLocalStore
 }
 
-function cargarListaTareas() {
+(function cargarListaTareas() {
   ObtenerLocalStore()
-  //este metodo sirve para recorrer los elementos que se encuentran en el localStore donde crea un objeto item y a su vez llama a su metodo interno crearDiv y le agrega las tareas
   for (tar of datosLocalStore) {
     let item = new Item(tar)
   }
-}
-
+})()
 
 function eliminarLocalStore(input) {
-  console.log(datosLocalStore)
-
   ObtenerLocalStore()
-  let arrayP =[]
-  
-  //recorro y filtro el array dando nuevos valores menos el de la tarea
-  for(let dato of datosLocalStore){
-    if(dato != input){
-      arrayP.push(dato)
-    }
-  }
-  console.log(arrayP);  
-  
-
-//remuevo los datos de localStore
+  datosLocalStore = datosLocalStore.filter(t => t != input)
+  datosLocalStore = JSON.stringify(datosLocalStore)
+  localStorage.setItem('tareas', datosLocalStore)
+}
+function editarLocalStore() {
   localStorage.removeItem('tareas')
-  // vuelvo a recorrer el array de datosLocalStore 
-  for (tar of arrayP) {
-    saveTask(tar) //agrego a LocalStore
+  for (valor of document.querySelectorAll('.item-input')) {
+    saveTask(valor.value)
   }
 }
 
-inputTarea.addEventListener("keyup",function(ev){
-  if(ev.keyCode  === 13){
+inputTarea.addEventListener("keyup", function (ev) {
+  if (ev.keyCode === 13) {
     chequearInput()
   }
 })
 
-cargarListaTareas()
+
 
 
 
